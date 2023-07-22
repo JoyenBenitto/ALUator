33fba014a251f7b1ed92abaf27c617913df3d0fe
Joyen Benitto <joyen.benitto12@gmail.com>
2023-07-22 19:19:08 +0530

    added parameters

--- ALUator/generator.py
+++ ALUator/generator.py
@@ -1,6 +1,7 @@
 import template as temp
 import yaml
 import time
+import math
 
 def generator(build_dir):
     ti = time.ctime()
@@ -8,15 +9,23 @@ def generator(build_dir):
     with open('test/pass/pass1.yaml') as f:
         data = yaml.load(f, Loader= yaml.SafeLoader)
 
+    case_statements = ""
+    for cases in data['ALU']:
+        binary = bin(cases)
+        case_statements += "\t\t\t" + temp.template_case.format(
+            control_width = math.ceil(math.log2(len(data['ALU']))),
+            encoding = binary.replace("0b",""),
+            operation = data['ALU'][cases]
+        )
+        pass
     
-    
-    with open(f"{build_dir}/generated.py","w") as fp:
+    with open(f"{build_dir}/alu.sv","w") as fp:
         # Creating the groud  plane and the substrate
         fp.write(temp.template_alu.format(
             time = "",
-            parameter_size ="",
-            control_width="",
-            cases="",
+            parameter_size = data['Alu_size'],
+            control_width= math.ceil(math.log2(len(data['ALU']))),
+            cases= case_statements,
             at_default=""
         ))
         
--- ALUator/template.py
+++ ALUator/template.py
@@ -14,7 +14,7 @@ output logic [size-1:0]out
 always_comb
     begin
         case(control)
-            {cases}
+{cases}
             default: out = {parameter_size}'b{at_default};
         endcase
     end
@@ -22,6 +22,5 @@ endmodule
 
 '''
 
-template_case='''
-{control_width}'b{encoding}: out = src1 {operation} src2;
+template_case='''{control_width}'b{encoding}: out = src1 {operation} src2;
 '''
